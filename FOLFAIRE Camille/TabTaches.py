0b21ebd7df36056bed7ca785bd98c3283f481f77
camilledelot <147487276+camilledelot@users.noreply.github.com>
2023-11-14 12:22:21 +0100

    Update TabTaches.py

--- FOLFAIRE Camille/TabTaches.py
+++ FOLFAIRE Camille/TabTaches.py
@@ -1,9 +1,15 @@
+import json 
+from Tache import *
+import copy
+
 class TabTaches:
     def __init__(self):
         self.taches = [] # Crée une liste vide pour stocker les tâches
 
     def ajouter_tache(self, tache):
-        self.taches.append(tache)  # Ajoute une tâche à la liste
+        inter = Tache('',0,'','')
+        inter = copy.deepcopy(tache)
+        self.taches.append(inter)  # Ajoute une tâche à la liste
 
     def afficher_taches(self):
         for tache in self.taches:
@@ -15,3 +21,19 @@ class TabTaches:
             print(f"Tâche '{tache.nom}' supprimée avec succès.")
         else:
             print(f"Tâche '{tache.nom}' n'a pas été trouvée dans la liste.")
+
+    def enregistrer_json(self, nom_fichier):
+        liste_taches = [tache.to_dict() for tache in self.taches]
+        with open(nom_fichier, 'w') as fichier:
+            json.dump(liste_taches, fichier, indent=4)
+
+    def charger_json(self, nom_fichier):
+        try:
+            with open(nom_fichier, 'r', encoding='utf-8') as fichier:
+                liste_taches = json.load(fichier)
+                self.taches = [Tache.from_dict(tache_dict) for tache_dict in liste_taches]
+        except FileNotFoundError:
+            print(f"Fichier '{nom_fichier}' non trouvé. Aucune tâche chargée.")
+
+    def tri_prio(self):
+        self.taches.sort(key=lambda tache: tache.prio, reverse=True)
